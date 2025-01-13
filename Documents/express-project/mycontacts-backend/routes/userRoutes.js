1f606e76e1294df4556be4fd4381e0c88c6657ff
StevenOng <steven@skyconsultancymns.com>
2025-01-13 15:24:36 +0800

    2nd

--- Documents/express-project/mycontacts-backend/routes/userRoutes.js
+++ Documents/express-project/mycontacts-backend/routes/userRoutes.js
@@ -1,17 +1,12 @@
 const express = require("express");
+const { registerUser, loginUser, currentUser } = require("../controllers/userController");
 
 const router = express.Router();
 
-router.post("/regiter", (req, res) => {
-    res.json({message: "Register the user"});
-});
+router.post("/register", registerUser);
 
-router.post("/login", (req, res) => {
-    res.json({message: "Login user"});
-});
+router.post("/login", loginUser);
 
-router.post("/current", (req, res) => {
-    res.json({message: "Current user information"});
-});
+router.get("/current", currentUser);
 
 module.exports = router;
